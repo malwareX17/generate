1c525d0fe3226f144e26216c7568bbd39b9392c7
malwareX17 <malwareX17@gmail.com>
2026-03-31 00:37:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello, World!");
+
+document.getElementById('createFile').onclick = function() {
+      const text = "This text file was generated from your web app!";
+      const blob = new Blob([text], { type: "text/plain" });
+      const anchor = document.createElement("a");
+      
+      anchor.download = "generated_file.txt";
+      anchor.href = window.URL.createObjectURL(blob);
+      anchor.target = "_blank";
+      anchor.style.display = "none";
+      
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+    };
